ecfe6c0958fbaff280d3e21ac7e53175ee33b0dc
Ryuu Mitsuki <dhefam31@proton.me>
2026-05-04 22:35:26 +0700

    refactor: Expose several modules
    These modules include:
      - `builtins/date`
      - `builtins/error`
      - `utils` (with namespace 'utils')

--- src/typeguard.ts
+++ src/typeguard.ts
@@ -7,9 +7,12 @@
 
 export * from './async/promise';
 export * from './builtins/array';
+export * from './builtins/date';
+export * from './builtins/error';
 export * from './builtins/function';
 export * from './builtins/object';
 export * from './builtins/primitive';
 export * from './envs/env';
 
-// ! DO NOT export `utils`
+// ! DO NOT export `utils` witbout namespace
+export * as utils from './utils';
